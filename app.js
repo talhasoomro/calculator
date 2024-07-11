21d18f1afd62e3fbce217b006da03f257cc264b7
talhasoomro <146392143+talhasoomro@users.noreply.github.com>
2024-07-11 22:59:33 +0500

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get('/', function(req, res) {
 
 
 
-app.listen(3000, function () {
-  console.log('App listening on port 3000!');
+app.listen(5000, function () {
+  console.log('App listening on port 5000!');
 });
 
